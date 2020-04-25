ce695a0cb24fcb90bf01fa883de0d9915a680804
Saidmurodov B.R <begmurod1999@mail.ru>
2020-04-26 01:08:01 +0500

    Добавление WinForm и UC для типо (type)

---
+++ Kursach/UI/TypeUC.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.Xpo;
+using Kursach.UI.WinForm;
+using Kursach.DB.DBKursach;
+
+namespace Kursach.UI
+{
+    public partial class TypeUC : DevExpress.XtraEditors.XtraUserControl
+    {
+        #region Для блокировки кнопок. Нужно вынести в отдельный абстрактный класс
+        public event EventHandler lockButtons;
+        public event EventHandler unlockButtons;
+
+        public virtual void fireLockButtons(EventArgs e)
+        {
+            if (lockButtons != null)
+                lockButtons(this, e);
+        }
+
+
+        public virtual void fireUnlockButtons(EventArgs e)
+        {
+            if (unlockButtons != null)
+                unlockButtons(this, e);
+        }
+        #endregion
+        public TypeUC()
+        {
+            InitializeComponent();
+        }
+        public TypeUC(Control _parent)
+        {
+            InitializeComponent();
+            Parent = _parent;
+
+            lockButtons += new EventHandler(OnDimensionSetLockButtons);
+            unlockButtons += new EventHandler(OnDimensionUnlockButtons);
+        }
+        public void OnDimensionSetLockButtons(object sender, EventArgs e)
+        {
+            barButtonItemView.Enabled = false;
+            barButtonItemEdit.Enabled = false;
+            barButtonItemDelete.Enabled = false;
+        }
+
+        public void OnDimensionUnlockButtons(object sender, EventArgs e)
+        {
+            barButtonItemView.Enabled = true;
+            barButtonItemEdit.Enabled = true;
+            barButtonItemDelete.Enabled = true;
+        }
+
+        private void TypeUC_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+        private void loadData()
+        {
+            using (UnitOfWork u = new UnitOfWork())
+            {
+                //TODO сделать фильтр по критериям
+                DevExpress.Xpo.DB.SelectedData _typevendorData = u.ExecuteQuery(string.Format(@"
+               SELECT [GUID]
+      ,[NameType]
+  FROM [device].[Type]
+  WHERE [DeletedDate] is null"
+                ));
+                typeDataView.LoadData(_typevendorData);
+            }
+            GC.Collect();
+        }
+
+        private void barButtonItemAdd_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            using (TypeForm addForm = new TypeForm())
+            {
+                addForm.ShowDialog();
+                addForm.Dispose();
+            }
+            loadData();
+        }
+
+        private void barButtonItemView_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+            TypeForm viewForm = new TypeForm();
+            viewForm.currentTypeGuid = (Guid)typeGridView.GetFocusedRowCellValue("GUID");
+            viewForm.edit = true;
+            viewForm.view = true;
+            viewForm.ShowDialog();
+        }
+
+        private void barButtonItemEdit_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            using (TypeForm editForm = new TypeForm())
+            {
+                editForm.currentTypeGuid = (Guid)typeGridView.GetFocusedRowCellValue("GUID");
+                editForm.edit = true;
+                editForm.ShowDialog();
+            }
+            loadData();
+        }
+
+        private void barButtonItemDelete_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            using (UnitOfWork u = new UnitOfWork())
+            {
+                device_Type currentType = u.GetObjectByKey<device_Type>((Guid)typeGridView.GetFocusedRowCellValue("GUID"));
+                DialogResult d = XtraMessageBox.Show(string.Format("Удалить тип {0}?", currentType.NameType), "Подтверждение действия", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (d == DialogResult.Yes)
+                {
+                    currentType.DeletedDate = DateTime.Now;
+                    currentType.Save();
+                    u.CommitChanges();
+                }
+            }
+            loadData();
+        }
+
+        private void typeGridView_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            if (e.FocusedRowHandle < 0)
+            {
+                fireLockButtons(new EventArgs());
+            }
+            else
+            {
+                fireUnlockButtons(new EventArgs());
+            }
+        }
+    }
+}

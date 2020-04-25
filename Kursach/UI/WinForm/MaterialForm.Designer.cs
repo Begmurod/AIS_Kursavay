7fde10c2dfacd5150a803159e723aa8621549e26
Saidmurodov B.R <begmurod1999@mail.ru>
2020-04-26 01:06:39 +0500

    добавление WinForm и UC для Материалы (material).

---
+++ Kursach/UI/WinForm/MaterialForm.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Kursach.UI.WinForm
+{
+    partial class MaterialForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraEditors.DXErrorProvider.CompareAgainstControlValidationRule compareAgainstControlValidationRule1 = new DevExpress.XtraEditors.DXErrorProvider.CompareAgainstControlValidationRule();
+            DevExpress.XtraEditors.DXErrorProvider.CompareAgainstControlValidationRule compareAgainstControlValidationRule2 = new DevExpress.XtraEditors.DXErrorProvider.CompareAgainstControlValidationRule();
+            DevExpress.XtraEditors.DXErrorProvider.CompareAgainstControlValidationRule compareAgainstControlValidationRule3 = new DevExpress.XtraEditors.DXErrorProvider.CompareAgainstControlValidationRule();
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.cancelButton = new DevExpress.XtraEditors.SimpleButton();
+            this.addButton = new DevExpress.XtraEditors.SimpleButton();
+            this.nameTextEdit = new DevExpress.XtraEditors.TextEdit();
+            this.volumeTextEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.unitOfMeasurementTextEdit = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem7 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem8 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.dxValidationProvider = new DevExpress.XtraEditors.DXErrorProvider.DXValidationProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nameTextEdit.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeTextEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.unitOfMeasurementTextEdit.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dxValidationProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.cancelButton);
+            this.layoutControl1.Controls.Add(this.addButton);
+            this.layoutControl1.Controls.Add(this.nameTextEdit);
+            this.layoutControl1.Controls.Add(this.volumeTextEdit1);
+            this.layoutControl1.Controls.Add(this.unitOfMeasurementTextEdit);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(597, 225, 358, 350);
+            this.layoutControl1.OptionsView.UseDefaultDragAndDropRendering = false;
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(444, 120);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(302, 84);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(130, 22);
+            this.cancelButton.StyleController = this.layoutControl1;
+            this.cancelButton.TabIndex = 11;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(157, 84);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(141, 22);
+            this.addButton.StyleController = this.layoutControl1;
+            this.addButton.TabIndex = 10;
+            this.addButton.Text = "Сохранить";
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // nameTextEdit
+            // 
+            this.nameTextEdit.Location = new System.Drawing.Point(120, 12);
+            this.nameTextEdit.Name = "nameTextEdit";
+            this.nameTextEdit.Size = new System.Drawing.Size(312, 20);
+            this.nameTextEdit.StyleController = this.layoutControl1;
+            this.nameTextEdit.TabIndex = 4;
+            compareAgainstControlValidationRule1.ErrorText = "Данное поле не может быть пустым!";
+            this.dxValidationProvider.SetValidationRule(this.nameTextEdit, compareAgainstControlValidationRule1);
+            // 
+            // volumeTextEdit1
+            // 
+            this.volumeTextEdit1.Location = new System.Drawing.Point(120, 36);
+            this.volumeTextEdit1.Name = "volumeTextEdit1";
+            this.volumeTextEdit1.Size = new System.Drawing.Size(312, 20);
+            this.volumeTextEdit1.StyleController = this.layoutControl1;
+            this.volumeTextEdit1.TabIndex = 4;
+            compareAgainstControlValidationRule2.ErrorText = "Данное поле не может быть пустым!";
+            this.dxValidationProvider.SetValidationRule(this.volumeTextEdit1, compareAgainstControlValidationRule2);
+            // 
+            // unitOfMeasurementTextEdit
+            // 
+            this.unitOfMeasurementTextEdit.Location = new System.Drawing.Point(120, 60);
+            this.unitOfMeasurementTextEdit.Name = "unitOfMeasurementTextEdit";
+            this.unitOfMeasurementTextEdit.Size = new System.Drawing.Size(312, 20);
+            this.unitOfMeasurementTextEdit.StyleController = this.layoutControl1;
+            this.unitOfMeasurementTextEdit.TabIndex = 4;
+            compareAgainstControlValidationRule3.ErrorText = "Данное поле не может быть пустым!";
+            this.dxValidationProvider.SetValidationRule(this.unitOfMeasurementTextEdit, compareAgainstControlValidationRule3);
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem7,
+            this.layoutControlItem8,
+            this.emptySpaceItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem3});
+            this.layoutControlGroup1.Name = "Root";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(444, 120);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.nameTextEdit;
+            this.layoutControlItem1.CustomizationFormText = "Название материала";
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(424, 24);
+            this.layoutControlItem1.Text = "Название материала";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(105, 13);
+            // 
+            // layoutControlItem7
+            // 
+            this.layoutControlItem7.Control = this.addButton;
+            this.layoutControlItem7.Location = new System.Drawing.Point(145, 72);
+            this.layoutControlItem7.Name = "layoutControlItem7";
+            this.layoutControlItem7.Size = new System.Drawing.Size(145, 28);
+            this.layoutControlItem7.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem7.TextVisible = false;
+            // 
+            // layoutControlItem8
+            // 
+            this.layoutControlItem8.Control = this.cancelButton;
+            this.layoutControlItem8.Location = new System.Drawing.Point(290, 72);
+            this.layoutControlItem8.Name = "layoutControlItem8";
+            this.layoutControlItem8.Size = new System.Drawing.Size(134, 28);
+            this.layoutControlItem8.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem8.TextVisible = false;
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.AllowHotTrack = false;
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 72);
+            this.emptySpaceItem1.Name = "emptySpaceItem1";
+            this.emptySpaceItem1.Size = new System.Drawing.Size(145, 28);
+            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.volumeTextEdit1;
+            this.layoutControlItem2.ControlAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.layoutControlItem2.CustomizationFormText = "Объем материала";
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 24);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(424, 24);
+            this.layoutControlItem2.Text = "Объем материала";
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(105, 13);
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.unitOfMeasurementTextEdit;
+            this.layoutControlItem3.ControlAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.layoutControlItem3.CustomizationFormText = "Единица измерения";
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 48);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(424, 24);
+            this.layoutControlItem3.Text = "Единица измерения";
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(105, 13);
+            // 
+            // MaterialForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 120);
+            this.Controls.Add(this.layoutControl1);
+            this.MinimumSize = new System.Drawing.Size(446, 152);
+            this.Name = "MaterialForm";
+            this.Text = "Материал";
+            this.Load += new System.EventHandler(this.MaterialForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nameTextEdit.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeTextEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.unitOfMeasurementTextEdit.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dxValidationProvider)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraEditors.SimpleButton cancelButton;
+        private DevExpress.XtraEditors.SimpleButton addButton;
+        private DevExpress.XtraEditors.TextEdit nameTextEdit;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem7;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem8;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+        private DevExpress.XtraEditors.DXErrorProvider.DXValidationProvider dxValidationProvider;
+        private DevExpress.XtraEditors.TextEdit volumeTextEdit1;
+        private DevExpress.XtraEditors.TextEdit unitOfMeasurementTextEdit;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+    }
+}
---
+++ Kursach/UI/WinForm/MaterialForm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.Xpo;
+using Kursach.DB.DBKursach;
+
+namespace Kursach.UI.WinForm
+{
+    public partial class MaterialForm : DevExpress.XtraEditors.XtraForm
+    {
+        public bool edit;//при true режим формы переходит в редактирование (это надо еще сделать...)
+        public bool view;
+        public Guid currentMaterialGuid;
+
+        private device_Material currentMaterial;
+
+        bool formValid = true;//После реализации метода validForm поменять на false
+        private UnitOfWork uow = new UnitOfWork();
+        public MaterialForm()
+        {
+            InitializeComponent();
+        }
+        public MaterialForm(UnitOfWork u)
+        {
+            InitializeComponent();
+            uow = u;
+        }
+
+        private void MaterialForm_Load(object sender, EventArgs e)
+        {
+            initForm();
+        }
+        void initForm()
+        {
+            if (edit)
+            {
+                currentMaterial = uow.GetObjectByKey<device_Material>(currentMaterialGuid);
+            }
+            else
+            {
+                currentMaterial = new device_Material(uow);
+            }
+
+            #region Настройка внешнего вида
+            if (edit)
+            {
+                Text = string.Format("Редактирование единицы {0} ", currentMaterial.Name);
+                addButton.Text = "Сохранить";
+            }
+
+            if (view)
+            {
+                Text = string.Format("Просмотр единицы {0} ", currentMaterial.Name);
+                nameTextEdit.ReadOnly = true;
+                volumeTextEdit1.ReadOnly = true;
+                unitOfMeasurementTextEdit.ReadOnly = true;
+                layoutControlItem7.Visibility = DevExpress.XtraLayout.Utils.LayoutVisibility.Never;
+            }
+            if (edit)
+            {
+                nameTextEdit.Text = currentMaterial.Name;
+                volumeTextEdit1.EditValue = currentMaterial.Volume;
+                unitOfMeasurementTextEdit.Text = currentMaterial.UnitOfMeasurement;
+            }
+        }
+        #endregion
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            validForm();
+            SaveMachine();
+            uow.CommitChanges();//Сохранение объекта в БД       
+            Close();
+        }
+        void SaveMachine()
+        {
+            if (formValid)
+            {
+                currentMaterial.Name = nameTextEdit.Text;
+                currentMaterial.Volume = Convert.ToDecimal( volumeTextEdit1.Text);
+                currentMaterial.UnitOfMeasurement = unitOfMeasurementTextEdit.Text;
+                
+                if (edit)
+                {
+                    currentMaterial.DateOfChange = DateTime.Now;
+                    currentMaterial.Save();
+                }
+                uow.CommitChanges();//Сохранение объекта в БД  
+                Close();
+            }
+
+        }
+        private void validForm()
+        {
+            //TODO  Сделать валидацию формы!!!
+            formValid = false;
+
+            List<string> notExistData = new List<string>();
+
+            formValid = dxValidationProvider.Validate();
+            foreach (Control c in dxValidationProvider.GetInvalidControls())
+            {
+                notExistData.Add(dxValidationProvider.GetValidationRule(c).ErrorText);
+            }
+            if (!formValid && notExistData.Count != 0)
+            {
+                int i = 1;
+                string str = string.Empty;
+                foreach (string s in notExistData)
+                {
+                    if (i == 1)
+                        str = string.Format("\n{0}. {1}", i, s);
+                    else
+                        str = string.Format("{0}\n{1}. {2}", str, i, s);
+                    i++;
+                }
+                str = string.Format("Для сохранения записи не хватает данных: {0}", str);
+                XtraMessageBox.Show(str, "Ввод недостающих данных", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+        
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            uow.Dispose();
+            Close();
+        }
+    }
+}
